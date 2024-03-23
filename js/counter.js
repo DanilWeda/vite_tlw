7f43ed0b1e0cd10f6acb03f8814d32da526e7c1c
= <danil.grigorets@gmail.com>
2024-03-23 20:32:15 +0200

    Linter error

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,7 @@
 export function setupCounter(element) {
   let counter = 1;
   const setCounter = (count) => {
-    counter = count;
+    counter = count
     element.innerHTML = `count is ${counter}`;
   };
   element.addEventListener("click", () => setCounter(counter + 1));
