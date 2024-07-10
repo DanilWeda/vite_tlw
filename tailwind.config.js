ad435462a9b146215bbba49ef035206dee546891
DanilWeda <danil.grigorets@gmail.com>
2024-07-10 12:00:23 +0300

    Added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    fontSize: {
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["20px", "28px"],
+      xl: ["24px", "32px"],
+      xxl: ["30px", "38px"],
+      "2xl": ["38px", "46px"],
+      "3xl": ["52px", "58px"],
+    },
+    extend: {
+      colors: {
+        "color-black": "#000000",
+        primary: "#FFF3E3",
+        secondary: "#cc9933",
+      },
+    },
   },
   plugins: [],
 };
